2b727af40304a40ac6916ace8a395307014d0962
Muaaz <yourssharky@gmail.com>
2023-11-27 13:59:20 +0500

    worst experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
+import * as ml5 from "ml5"
    function App() {
   const [Image,setImage]=useState("")
+  const [Results,setResults]=useState([])
   let Ref =useRef()
-let imageClassifier ;  
-  useEffect(() => {
-     imageClassifier = ml5.imageClassifier('MobileNet', () => {
-      console.log('Model Loaded!');
-    });
-classifyImage()
- 
-  }, [Image])
   const classifyImage = () => {
+    let imageClassifier = ml5.imageClassifier('MobileNet', () => {
+     console.log('Model Loaded!');
+   });
       imageClassifier.predict(Ref.current, (err, results) => {
+        
         if (err) {
           console.error(err);
         } else {
-          console.log(results);
+          setResults(results);
       }
     });
 
@@ -26,7 +24,7 @@ classifyImage()
 return(
   <>
 <main className='w-[300px] h-[300px] bg-black object-contain'>
-        <img src={Image} ref={Ref} alt="image" className='w-full h-full ' />
+        <img src={Image} ref={Ref} alt="image" className='w-full h-full' />
     </main>
     <input type="file" name="" id="Image" onChange={(e)=>{
         let ImageURl  = URL.createObjectURL(e.target.files[0])
@@ -35,6 +33,15 @@ return(
       }
         } hidden/>
     <label htmlFor="Image" className=' p-2 cursor-pointer '>Upload Image</label>
+    <div className=''>
+<h1>Results</h1>
+    {
+      Results.length !==0&&
+      Results.map(elm=>{
+        return elm.label
+      })
+    }
+    </div>
 
   </>
 )
